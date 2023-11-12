4da9ffbbd0df4c8c52aa811f5613a07cb899fdf3
Javier Carrasco <javier.carrasco.cruz@gmail.com>
2023-11-12 20:11:08 +0100

    scripts: local scripts to configure, compile and load raspberry images

---
+++ scripts/local/configure.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x #echo on
+
+#64-bit kernel config for Raspberry Pi Zero 2 W
+KERNEL=kernel8
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
+#directories to mount the SD partitions
+mkdir -p mnt/{fat32,ext4}
